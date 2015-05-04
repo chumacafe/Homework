73d6a2ccef78ce0735adea3cebf459974db3081a
chumacafe <chumacafe@yahoo.co.jp>
2015-05-04 20:57:48 +0900

    Calculator commit

--- Homework/Homework/ViewController.swift
+++ Homework/Homework/ViewController.swift
@@ -13,59 +13,54 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Labelを作成.
-        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
+        let button1 = makeButton(25,y:300)
+        let button2 = makeButton(25,y:400)
+        let button3 = makeButton(25,y:500)
+        let button4 = makeButton(25,y:600)
+        let button5 = makeButton(25,y:700)
         
-        // 背景をオレンジ色にする.
-        myLabel.backgroundColor = UIColor.blackColor()
+        button1.setTitle("1", forState: .Normal)
+        button2.setTitle("2", forState: .Normal)
+        button3.setTitle("3", forState: .Normal)
+        button4.setTitle("4", forState: .Normal)
         
-        // 枠を丸くする.
-        myLabel.layer.masksToBounds = true
         
-        // コーナーの半径.
-        myLabel.layer.cornerRadius = 30.0
         
-        // Labelに文字を代入.
-        myLabel.text = "Welcome to Kinniku!!"
+    
+        self.view.addSubview(button1)
+        self.view.addSubview(button2)
+        self.view.addSubview(button3)
+        self.view.addSubview(button4)
+        self.view.addSubview(button5)
         
-        // 文字の色を白にする.
-        myLabel.textColor = UIColor.whiteColor()
         
-        // 文字の影の色をグレーにする.
-        myLabel.shadowColor = UIColor.grayColor()
-        
-        // Textを中央寄せにする.
-        myLabel.textAlignment = NSTextAlignment.Center
-        
-        // 配置する座標を設定する.
-        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 200)
-        
-        // ViewにLabelを追加.
-        self.view.addSubview(myLabel)
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    func makeButton(x: CGFloat,y:CGFloat) -> UIButton {
         
         let button = UIButton()
         
         //表示されるテキスト
-        button.setTitle("押してー!", forState: .Normal)
+        button.setTitle("1", forState: .Normal)
         
         //テキストの色
         button.setTitleColor(UIColor.blueColor(), forState: .Normal)
         
         //タップした状態のテキスト
-        button.setTitle("筋肉！", forState: .Highlighted)
+        button.setTitle("よっ、メロン肩！", forState: .Highlighted)
         
         //タップした状態の色
-        button.setTitleColor(UIColor.redColor(), forState: .Highlighted)
+        button.setTitleColor(UIColor.blackColor(), forState: .Highlighted)
         
         //サイズ
-        button.frame = CGRectMake(0, 0, 300, 50)
+        button.frame = CGRectMake(0, 0, 100, 100)
         
         //配置場所
-        button.layer.position = CGPoint(x: self.view.frame.width/2, y:100)
+        button.layer.position = CGPoint(x:x, y:y)
         
         //背景色
-        button.backgroundColor = UIColor(red: 0.7, green: 0.2, blue: 0.2, alpha: 0.2)
+        button.backgroundColor = UIColor(red: 0.5, green: 0, blue: 0, alpha: 0.6)
         
         //角丸
         button.layer.cornerRadius = 10
@@ -77,8 +72,9 @@ class ViewController: UIViewController {
         //button.addTarget(self, action: "tapped:", forControlEvents:.TouchUpInside)
         
         //viewにボタンを追加する
-        self.view.addSubview(button)    }
-
+        return button
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
